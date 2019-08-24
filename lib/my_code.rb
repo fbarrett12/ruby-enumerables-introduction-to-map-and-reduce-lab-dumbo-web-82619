0cfb8cb7b660ce741efaebe7ec9f325efb4a26cf
Fredrick Barrett <f.barrett12@gmail.com>
2019-08-24 16:54:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,7 +45,16 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-    
+  counter = 0
+  
+  while counter < source_array.length
+    if source_array[counter] ==  true do 
+      return true
+    else 
+      return false
+    end
+  end
+end
 end
 
 def reduce_to_any_true(source_array)
